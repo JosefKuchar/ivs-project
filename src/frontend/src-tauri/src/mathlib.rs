8dec844001919c08d23a8364da7721237044d61c
Josef Kuchař <josef.kuchar267@gmail.com>
2022-02-14 22:07:45 +0100

    Switch to dec crate for decimal precision

--- src/frontend/src-tauri/src/mathlib.rs
+++ src/frontend/src-tauri/src/mathlib.rs
@@ -1,25 +1,25 @@
-pub fn add(a: f64, b: f64) -> f64 {
-  0.0
+use dec::*;
+
+// Number of digit precision
+//TODO: Determine this properly
+const PRECISION: usize = 200;
+
+// Typedef for simpler usage, also should be used outside mode
+pub type Dec = Decimal<PRECISION>;
+
+pub fn add(a: Dec, b: Dec) -> Dec {
+  unimplemented!()
 }
 
-pub fn subtract(a: f64, b: f64) -> f64 {
-  0.0
-}
-
-pub fn multiply(a: f64, b: f64) -> f64 {
-  0.0
-}
-
-pub fn divide(a: f64, b: f64) -> f64 {
-  0.0
-}
-
+#[cfg(test)]
 mod tests {
-    use super::*;
-    use float_cmp::*;
+  use dec::*;
 
-    #[test]
-    fn test_add() {
-        assert!(approx_eq!(f64, add(0.1, 0.2), 0.3));
-    }
+  #[test]
+  fn add() {
+    assert_eq!(
+      super::add(Decimal::from(0.1), Decimal::from(0.2)),
+      Decimal::from(0.3)
+    );
+  }
 }
--- src/frontend/src-tauri/src/stddev.rs
+++ src/frontend/src-tauri/src/stddev.rs
@@ -1,5 +1,35 @@
+use std::io;
+use std::io::Read;
 mod mathlib;
+use mathlib::*;
 
 fn main() {
+  // Buffer to store the whole input
+  let mut buffer = String::new();
+
+  // Read the whole input
+  io::stdin().read_to_string(&mut buffer).expect("Read error");
+
+  // Split the input by whitespace and parse
+  let numbers: Vec<Dec> = buffer
+    .split_whitespace()
+    .map(|s| s.parse().expect("Parsing error - invalid number"))
+    .collect();
+
+  // Calculate the standard deviation
+  let mut sum = Dec::zero();
+  let mut sd = Dec::zero();
+  // for number in numbers.iter() {
+  //   sum = add(sum, *number);
+  // }
+  // sum = divide(sum, numbers.len() as f64);
+  // for number in numbers.iter() {
+  //   sd = add(sd)
+  // }
+
+  for num in numbers {
+    println!("{}", num);
+  }
+
   println!("Hello world!");
 }
