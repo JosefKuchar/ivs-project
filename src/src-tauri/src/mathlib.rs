b58c5287ce27d56dd9dff214402288119fba96d6
SimonNeckar <smisek159@gmail.com>
2022-03-24 23:51:20 +0100

    tests baseline

--- src/src-tauri/src/mathlib.rs
+++ src/src-tauri/src/mathlib.rs
@@ -47,7 +47,138 @@ mod tests {
   fn add() {
     assert_eq!(
       super::add(Decimal::from(0.1), Decimal::from(0.2)),
-      Decimal::from(0.3)
+      Decimal::from(0.3);
     );
+  }
+
+  #[test]
+  fn add() {
+    assert_eq! (
+        super::add(Decimal::from(7), Decimal::from(-5)),
+        Decimal::from(2);
+    );
+  }
+  
+  #[test] // potreba pojmout celle R jako mozny vysledek
+  fn substract() {
+      assert_eq! (
+          super::substract(Decimal::from(0), Decimal::from(0)),
+          Decimal::from();
+      );
+  }
+
+  #[test]
+  fn substract() {
+      assert_eq! (
+          super::substract(Decimal::from(0), Decimal::((-1) * from(0))),
+          Decimal::from(0);
+      );
+  }
+
+
+  #[test]
+  fn multiply() {
+      assert_eq! (
+          super::multiply(Decimal::from(0), Decimal::((-1) * from(0))),
+          Decimal::((-1) * from(0));
+      );
+  }
+
+  #[test]
+  fn multiply() {
+      assert_eq! (
+          super::multiply(Decimal::from(0), Decimal::from(0)),
+          Decimal::from(0);
+      );
+  }
+
+  #[test]
+  fn multiply() {
+      assert_eq! (
+          super::multiply(Decimal::((-1) * from(0)), Decimal::((-1) * from(0))),
+          Decimal::from(0);
+      );
+  }
+
+  #[test]
+  fn divide() {
+      assert_eq! (
+          super::divide(Decimal::((-1) * from(0))), Decimal::from(0.000000000000000000000000001)),
+          Decimal::((-1) * from(0)));
+      );
+  }
+
+  #[test]
+  fn divide() {
+      assert_eq! (
+          super::divide(Decimal::from(0), Decimal::from(0.000000000000000000000000001)),
+          Decimal::from(0);
+      );
+  }
+
+  #[test]
+  fn divide() {
+      should_panic! (
+          super::divide(Decimal::from(0), Decimal::(0)),
+          Decimal::from(0));
+      );
+  }
+
+  #[test]  // potreba pojmout celle R jako mozny vysledek
+  fn pow() {
+      assert_eq! (
+          super::pow(Decimal::((-1) * from(0)), Decimal::from(-48951)),
+          Decimal::from();
+      );
+  }
+  
+  #[test]
+  fn pow() {
+      assert_eq! (
+          super::pow(Decimal::from(0), Decimal::from(-48951)),
+          Decimal::from(0));
+      );
+  }
+
+  
+
+  #[test]
+  fn root() {
+      should_panic! (
+          super::root(Decimal::from(-18884), Decimal::from(2)),
+          Decimal::from(0));
+      );
+  }
+
+  #[test]
+  fn root() {
+      assert_eq! (
+          super::root(Decimal::from(0), Decimal::from(1)),
+          Decimal::from(0));
+      );
+  }
+
+  #[test]
+  fn factorial() {
+      should_panic! (
+          super::factorial(Decimal::from(-1568)),
+          Decimal::from(1);
+      );
+  }
+
+  #[test]
+  fn factorial() {
+      assert_eq! (
+          super::factorial(Decimal::from(0) ),
+          Decimal::from(1);
+      );
+  }
+
+  #[test]
+  fn abs() {
+      assert_eq! (
+          super::abs(Decimal::from(-14556)),
+          Decimal::((-1)*from(-14556)));
+      );
   }
 }
