fb36f4767b940461beea550b731096a4d3d858b7
SimonNeckar <smisek159@gmail.com>
2022-03-29 02:12:28 +0200

    Add third version of math library tests.

--- src/src-tauri/src/mathlib.rs
+++ src/src-tauri/src/mathlib.rs
@@ -47,173 +47,262 @@ mod tests {
   fn add() {
     assert_eq!(
       super::add(Decimal::from(0.1), Decimal::from(0.2)),
-      Decimal::from(0.3));
+      Decimal::from(0.3)
+    );
+  }
+
+  #[test]
+  fn add() {
+    assert_eq!(
+      super::add(Decimal::from(2.1), Decimal::from(2)),
+      Decimal::from(4.1)
+    );
   }
 
   #[test]
   fn add() {
     assert_eq!(
       super::add(Decimal::from(8453216), Decimal::from(5462132)),
-      Decimal::from(13905348));
+      Decimal::from(13905348)
+    );
   }
-
 
   #[test]
   fn add() {
-    assert_eq! (
-        super::add(Decimal::from(7), Decimal::from(-5)),
-        Decimal::from(2));
-  }
-  
-  #[test] 
-  fn substract() {
-      assert_eq! (
-          super::substract(Decimal::from(5), Decimal::from(10)),
-          Decimal::from(-5));
-  }
-  
-  #[test] 
-  fn substract() {
-      assert_eq! (
-          super::substract(Decimal::from(0.7), Decimal::from(0.6)),
-          Decimal::from(0.1));
+    assert_eq!(
+      super::add(Decimal::from(0), Decimal::from(0.2)),
+      Decimal::from(0.2)
+    );
   }
 
   #[test]
-  fn substract() {
-      assert_eq! (
-          super::substract(Decimal::from(0), Decimal::from(-23)),
-          Decimal::from(23));
+  fn add() {
+    assert_eq!(
+      super::add(Decimal::from(7), Decimal::from(-15)),
+      Decimal::from(-8)
+    );
   }
 
+  #[test]
+  fn subtract() {
+    assert_eq!(
+      super::subtract(Decimal::from(5), Decimal::from(10)),
+      Decimal::from(-5)
+    );
+  }
+
+  #[test]
+  fn subtract() {
+    assert_eq!(
+      super::add(Decimal::from(2), Decimal::from(0.3)),
+      Decimal::from(1.7)
+    );
+  }
+
+  #[test]
+  fn subtract() {
+    assert_eq!(
+      super::subtract(Decimal::from(0.7), Decimal::from(0.6)),
+      Decimal::from(0.1)
+    );
+  }
+
+  #[test]
+  fn subtract() {
+    assert_eq!(
+      super::subtract(Decimal::from(0), Decimal::from(-23)),
+      Decimal::from(23)
+    );
+  }
+
+  #[test]
+  fn subtract() {
+    assert_eq!(
+      super::subtract(Decimal::from(0), Decimal::from(69420)),
+      Decimal::from(-69420)
+    );
+  }
 
   #[test]
   fn multiply() {
-      assert_eq! 
-          super::multiply(Decimal::from(652), Decimal::(from(0)),
-          Decimal::from(0));
+    assert_eq!(
+      super::multiply(Decimal::from(652), Decimal::from(0)),
+      Decimal::from(0)
+    );
   }
 
   #[test]
   fn multiply() {
-      assert_eq! (
-          super::multiply(Decimal::from(8), Decimal::from(-32)),
-          Decimal::from(-256));
+    assert_eq!(
+      super::multiply(Decimal::from(7), Decimal::from(191)),
+      Decimal::from(1337)
+    );
   }
 
   #[test]
   fn multiply() {
-      assert_eq! (
-          super::multiply(Decimal::from(-7), Decimal::from(-6)),
-          Decimal::from(42));
+    assert_eq!(
+      super::multiply(Decimal::from(), Decimal::from(0)),
+      Decimal::from(0)
+    );
+  }
+
+  #[test]
+  fn multiply() {
+    assert_eq!(
+      super::multiply(Decimal::from(8), Decimal::from(-32)),
+      Decimal::from(-256)
+    );
+  }
+
+  #[test]
+  fn multiply() {
+    assert_eq!(
+      super::multiply(Decimal::from(-7), Decimal::from(-6)),
+      Decimal::from(42)
+    );
   }
 
   #[test]
   fn divide() {
-      assert_eq! (
-          super::divide(Decimal::from(48), Decimal::from(0.0005)),
-          Decimal::from(240000));
-    }
-
-  #[test]
-  fn divide() {
-      assert_eq! (
-          super::divide(Decimal::from(0), Decimal::from(6)),
-          Decimal::from(0));
+    assert_eq!(
+      super::divide(Decimal::from(48), Decimal::from(0.0005)),
+      Decimal::from(240000)
+    );
   }
 
   #[test]
   fn divide() {
-      should_panic! (
-          super::divide(Decimal::from(-531), Decimal::(0)),
-          Decimal::from(0));
+    assert_eq!(
+      super::divide(Decimal::from(0), Decimal::from(6)),
+      Decimal::from(0)
+    );
+  }
+
+  #[test]
+  #[should_panic]
+  fn divide() {
+    assert_eq!(
+      super::divide(Decimal::from(-531), Decimal::from(0)),
+      Decimal::from(0)
+    );
   }
 
   #[test]
   fn divide() {
-      assert_eq! (
-          super::divide(Decimal::from(-531), Decimal::(-15)),
-          Decimal::from(35.4));
+    assert_eq!(
+      super::divide(Decimal::from(-531), Decimal::from(-15)),
+      Decimal::from(35.4)
+    );
   }
 
-  #[test]  
+  #[test]
   fn pow() {
-      assert_eq! (
-          super::pow(Decimal::from(3), Decimal::from(-5)),
-          Decimal::from(0.00411522633744855967078189300412));
-  }
-  
-  #[test]
-  fn pow() {
-      assert_eq! (
-          super::pow(Decimal::from(0), Decimal::from(-48951)),
-          Decimal::from(0));
+    assert_eq!(
+      super::pow(Decimal::from(3), Decimal::from(-5)),
+      Decimal::from(0.00411522633744855967078189300412)
+    );
   }
 
   #[test]
   fn pow() {
-      assert_eq! (
-          super::pow(Decimal::from(-2), Decimal::from(3)),
-          Decimal::from(-8));
+    assert_eq!(
+      super::pow(Decimal::from(0), Decimal::from(-48951)),
+      Decimal::from(0)
+    );
+  }
+  #[test]
+  fn pow() {
+    assert_eq!(
+      super::pow(Decimal::from(2), Decimal::from(-4)),
+      Decimal::from(0.0625)
+    );
+  }
+  #[test]
+  fn pow() {
+    assert_eq!(
+      super::pow(Decimal::from(-5), Decimal::from(4)),
+      Decimal::from(625)
+    );
+  }
+  #[test]
+  fn pow() {
+    assert_eq!(
+      super::pow(Decimal::from(-3), Decimal::from(3)),
+      Decimal::from(-27)
+    );
   }
 
   #[test]
   fn root() {
-      assert_eq!     (
-          super::root(Decimal::from(-8), Decimal::from(3)),
-          Decimal::from(2));
+    assert_eq!(
+      super::root(Decimal::from(-8), Decimal::from(3)),
+      Decimal::from(-2)
+    );
   }
 
   #[test]
   fn root() {
-      assert_eq! (
-          super::root(Decimal::from(0), Decimal::from(2)),
-          Decimal::from(0));
+    assert_eq!(
+      super::root(Decimal::from(0), Decimal::from(2)),
+      Decimal::from(0)
+    );
   }
 
   #[test]
+  #[should_panic]
   fn root() {
-      should_panic! (
-          super::root(Decimal::from0(-4), Decimal::from(2)),
-          Decimal::from(2));
+    assert_eq!(
+      super::root(Decimal::from0(-4), Decimal::from(2)),
+      Decimal::from(2)
+    );
+  }
 
+  #[test]
+  #[should_panic]
+  fn root() {
+    assert_eq!(
+      super::root(Decimal::from0(-64), Decimal::from(6)),
+      Decimal::from(2)
+    );
+  }
+
+  #[test]
+  #[should_panic]
+  fn factorial() {
+    assert_eq!(super::factorial(Decimal::from(-1568)), Decimal::from(1));
   }
 
   #[test]
   fn factorial() {
-      should_panic! (
-          super::factorial(Decimal::from(-1568)),
-          Decimal::from(1));
+    assert_eq!(super::factorial(Decimal::from(0)), Decimal::from(1));
   }
 
   #[test]
   fn factorial() {
-      assert_eq! (
-          super::factorial(Decimal::from(0) ),
-          Decimal::from(1));
+    assert_eq!(
+      super::factorial(Decimal::from(15)),
+      Decimal::from(1, 307, 674, 368, 000)
+    );
   }
 
   #[test]
   fn factorial() {
-      assert_eq! (
-          super::factorial(Decimal::from(9) ),
-          Decimal::from(362,880));
+    assert_eq!(super::factorial(Decimal::from(9)), Decimal::from(362, 880));
   }
 
   #[test]
   fn abs() {
-      assert_eq! (
-          super::abs(Decimal::from(-14556)),
-          Decimal::from(14556));
-
+    assert_eq!(super::abs(Decimal::from(-14556)), Decimal::from(14556));
   }
 
   #[test]
   fn abs() {
-      assert_eq! (
-          super::abs(Decimal::from(856)),
-          Decimal::from(856));
-
+    assert_eq!(super::abs(Decimal::from(856)), Decimal::from(856));
   }
-  
+
+  #[test]
+  fn abs() {
+    assert_eq!(super::abs(Decimal::from(-0.8569)), Decimal::from(0.8569));
+  }
 }
