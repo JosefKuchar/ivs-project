2149e1b1adba44451604743a9fff38247282adb7
S1ro1 <matej.sirovatka@gmail.com>
2022-04-03 16:21:30 +0200

    Add base version of the math libraryy

--- src/src-tauri/src/mathlib.rs
+++ src/src-tauri/src/mathlib.rs
@@ -8,35 +8,63 @@ const PRECISION: usize = 200;
 pub type Dec = Decimal<PRECISION>;
 
 pub fn add(a: Dec, b: Dec) -> Dec {
-  unimplemented!()
+  a + b
 }
 
 pub fn subtract(a: Dec, b: Dec) -> Dec {
-  unimplemented!()
+  a - b
 }
 
 pub fn multiply(a: Dec, b: Dec) -> Dec {
-  unimplemented!()
+  a * b
 }
 
-pub fn divide(a: Dec, b: Dec) -> Option<Dec> {
-  unimplemented!()
+pub fn divide(a: Dec, b: Dec) -> Result<Dec, String> {
+  if b.is_zero() {
+    Err("Cannot divide by 0!".to_string())
+  } else {
+    Ok(a / b)
+  }
 }
 
-pub fn pow(a: Dec, b: Dec) -> Option<Dec> {
-  unimplemented!()
+pub fn pow(a: Dec, b: Dec) -> Result<Dec, String> {
+  let mut ctx = Context::<Dec>::default();
+  if b.exponent() != 0 {
+    Err("Only natural exponents are supported!".to_string())
+  } else {
+    let mut mut_a = a.clone();
+    let mut mut_b = b.clone();
+    ctx.pow(&mut mut_a, &mut mut_b);
+    Ok(mut_a)
+  }
 }
 
-pub fn root(a: Dec, b: Dec) -> Option<Dec> {
-  unimplemented!()
+pub fn root(a: Dec) -> Result<Dec, String> {
+  let mut ctx = Context::<Dec>::default();
+  if a.is_negative() {
+    Err("Invalid base of square root!".to_string())
+  } else {
+    let mut mut_a = a.clone();
+    ctx.sqrt(&mut mut_a);
+    Ok(mut_a)
+  }
 }
 
-pub fn factorial(a: Dec) -> Option<Dec> {
-  unimplemented!()
+pub fn factorial(a: Dec) -> Result<Dec, String> {
+  let mut ctx = Context::<Dec>::default();
+  if a.exponent() != 0 || a.is_negative() {
+    Err("Cannot calculate factorial!".to_string())
+  } else {
+    let b = ctx.try_into_u128(a).unwrap();
+    Ok(ctx.from_u128((1..=b).product()))
+  }
 }
 
 pub fn abs(a: Dec) -> Dec {
-  unimplemented!()
+  let mut ctx = Context::<Dec>::default();
+  let mut mut_a = a.clone();
+  ctx.abs(&mut mut_a);
+  mut_a
 }
 
 #[cfg(test)]
