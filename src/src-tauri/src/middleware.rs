a8632cc8198288485a42e8176ca0969264f305ea
Josef Kuchař <josef.kuchar267@gmail.com>
2022-04-18 10:45:20 +0200

    Create docs for middleware

--- src/src-tauri/src/middleware.rs
+++ src/src-tauri/src/middleware.rs
@@ -1,16 +1,27 @@
+//! Layer between frontend and backend
+//!
+//! Handles calls from frontend and returns operation result.
+
 use crate::mathlib::*;
 use serde::Deserialize;
 use tauri::command;
 
-// Define request JSON structure
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
+/// Request JSON structure
 pub struct RequestBody {
+  /// First operand
   a: String,
+  /// Second operand (only used on some operations)
   b: Option<String>,
+  /// Operation name (listed in mathlib)
   operation: String,
 }
 
+/// Number parsing wrapper
+///
+/// # Arguments
+/// * `string` - Number in string to be parsed
 fn handle_parse(string: String) -> Result<Dec, String> {
   match string.parse() {
     Ok(num) => Ok(num),
@@ -18,7 +29,13 @@ fn handle_parse(string: String) -> Result<Dec, String> {
   }
 }
 
-// Command handler
+/// Command handler
+///
+/// This gets called when frontend calls invoke via api.
+/// Returns operation result as string.
+///
+/// # Arguments
+/// * `payload` - JSON payload from frontend
 #[command]
 pub fn math_operation(payload: RequestBody) -> Result<String, String> {
   // Parse first operand
